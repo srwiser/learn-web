b715466c95942a930ecb0add4407309d6d2f7088
swapnil.rastogi <swapnil.rastogi@iref.com>
2016-03-09 16:40:51 +0530

    built a status update box

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var main = function() {
-    $('.icon-menu').click(function() {
-        $('.menu').animate({
-            left: '0px'
-        }, 200);
-        $('body').animate({
-            left: '285px'
-        }, 200);
+    $('.btn').click(function() {
+        var post = $('.status-box').val();
+        $('<li>').text(post).prependTo('.posts');
+        $('.status-box').val('');
+        $('.counter').text("140");
+        $('.btn').addClass('disabled');
     });
-    $('.icon-close').click(function() {
-        $('.menu').animate({
-            left: '-285px'
-        }, 200);
-        $('body').animate({
-            left: '0px'
-        }, 200);
+    $('.status-box').keyup(function(){
+        var postlength = $(this).val().length;
+        var charactersleft = 140 - postlength;
+        $('.counter').text(charactersleft);
+        if(charactersleft < 0) {
+            $('.btn').addClass('disabled');
+        }
+        else if(charactersleft === 140) {
+            $('.btn').addClass('disabled');
+        }
+        else {
+            $('.btn').removeClass('disabled');
+        }
     });
-};
+    //prevent empty messages on page load
+    $('.btn').addClass('disabled');
+}
 $(document).ready(main);
